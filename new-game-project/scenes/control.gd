43e50603551296df7a14bd46f9e315da05dfb57c
nigoi <87484366+nigoi@users.noreply.github.com>
2025-02-21 14:29:05 +0000

    respawn ball function working
    - spawns
    - updates lives and ball count

--- new-game-project/scenes/control.gd
+++ new-game-project/scenes/control.gd
@@ -1,28 +1,23 @@
 extends Control
-var lives = 3
-var balls = 0
+var lives = 4
+var balls = 1
 var score = 0
 
 func spawn_ball():
-	if balls > 1:
-		balls -= 1
-		#remove 1 life
-		if balls < 1:
-			lives -= 1
-			#update label
-		#if lives = 0 restart scene
-			if lives < 1:
-				get_tree().reload_current_scene()
-		#reset paddle
-	
-	#spawn ball
+	#removes ball and life drom count
+	lives -= 1
+	balls -= 1
+	#restarts scene if lives and balls are ou
+	if lives == 0 and balls == 0:
+		get_tree().reload_current_scene()
+	#else spawns ball, connects signal and adds ball to coun
 	else:
 		var ball = preload("res://scenes/ball.tscn")
 		var ball_inst = ball.instantiate()
 		add_child(ball_inst)
 		balls += 1
-		if ball_inst:
-			ball.connect("dead_ball", self.spawn_ball)
-	
+		ball_inst.connect("dead_ball", self.spawn_ball)
+		print("lives: ", lives, " balls: ", balls, " score: ", score)
+			
 func _ready() -> void:
 	spawn_ball()
